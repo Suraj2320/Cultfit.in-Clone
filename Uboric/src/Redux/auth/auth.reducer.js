146e145eb4149cbbee108b5fbaef5548f3dff8bf
aayan269 <ahmadaayan269@gmail.com>
2022-11-09 23:10:05 +0530

    added

---
+++ Uboric/src/Redux/auth/auth.reducer.js
@@ -0,0 +1,55 @@
+import {
+    LOGIN_ERROR,
+    LOGIN_REQUEST,
+    LOGIN_SUCCESS,
+    LOGOUT
+  } from "./auth.types";
+  
+  let token = localStorage.getItem("token");
+  const initial = {
+    isAuth: !!token,
+    token: token,
+    loading: false,
+    error: false
+  };
+  
+  export const authReducer = (state = initial, { type, payload }) => {
+    switch (type) {
+      case LOGIN_REQUEST: {
+        return {
+          ...state,
+          loading: true,
+          error: false
+        };
+      }
+      case LOGIN_SUCCESS: {
+        localStorage.setItem("token", payload);
+        return {
+          ...state,
+          token: payload,
+          isAuth: true,
+          loading: false,
+          error: false
+        };
+      }
+      case LOGIN_ERROR: {
+        return {
+          ...state,
+          loading: false,
+          error: true
+        };
+      }
+      case LOGOUT: {
+        localStorage.removeItem("token");
+        return {
+          ...state,
+          isAuth: false,
+          token: ""
+        };
+      }
+  
+      default:
+        return state;
+    }
+  };
+  
