236a253860ec8656f866fb21fa843212b6e301df
Vishal Raj <vishal.kumar4856@gmail.com>
2022-11-12 04:24:58 +0530

    care page added

--- cultfit/src/Components/Care/Care.jsx
+++ cultfit/src/Components/Care/Care.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import ImageSlider from "./ImageSlider";
-// import { SliderData } from "./SliderData";
 import Specialities from "./Specialities";
 import "./Care.scss";
 
