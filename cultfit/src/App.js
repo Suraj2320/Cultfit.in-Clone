f0ee9b4c63eb088bb3869ff31fc3fc9701f43c88
aayan269 <ahmadaayan269@gmail.com>
2022-12-23 16:23:51 +0530

    added cart

--- cultfit/src/App.js
+++ cultfit/src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import { Store } from './Components/Store/Store';
+import "./App.css";
+import { Store } from "./Components/Store/Store";
 // import Care from './Components/Care/Care';
 // import Footer from './Components/Footer/Footer';
 // import Home from './Components/Home/Home';
-import Navbar from './Components/navbar/Navbar'
-import AllRoutes from './Components/AllRoutes';
-import Footer from './Components/Footer/Footer';
+import Navbar from "./Components/navbar/Navbar";
+import AllRoutes from "./Components/AllRoutes";
+import Footer from "./Components/Footer/Footer";
+import Fitness1 from "./Components/Fitness/Fitness1";
+import { FitnessGif } from "./Components/Fitness/FitnessGif";
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
       <Navbar/>
       <AllRoutes/>
       <Footer />
+      
     </div>
   );
 }
